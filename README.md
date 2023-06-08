53cfc155d87c7fd4543ac80633607eeec36325bb
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-06-08 17:02:59 -0400

    added restcontroller and method handling annotations

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ what is pom.xml?
 pom is a XML file that contains information about the project and configuration details used by Maven to build the project. When executing a task or goal, Maven looks for the POM in the current directory. It reads the POM, gets the needed configuration information, then executes the goal
 
 
-
+What is tomcat?
+Provides a pure java HTTP web server environment in which Java code can also run. It is a Java web application server
 
 Spring boot folder layouts 
 
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,17 +2,23 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //makes it a spring boot application
 @SpringBootApplication
 
+//request handling methods 
+@RestController
 public class Main {
     public static void main(String[] args) {
         // calls the spring boot application 
         SpringApplication.run(Main.class, args);
 
-        // System.out.println("Hello");
-    
-        
+    }
+
+        @GetMapping("/greet")
+    public String greet(){
+        return "Hello";
     }
 }
